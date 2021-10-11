2f7139a3382db789b2ef535eaf72fd88eec20533
Lauan Borges Guermandi <lbguermandi@paschoalotto.com.br>
2021-10-11 04:29:43 -0300

    feat: add project files

---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Client
+{
+    class Program
+    {
+        private static readonly HttpClient _client = new();
+        static async Task Main(string[] _)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = await Loadkey();
+
+            // Recomendação no NIS e da Microsoft
+            var enc = new EncryptingCredentials(key, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes128CbcHmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = "issuer",
+                Audience = "audience",
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("card_number", "4000-0000-0000-0002"),
+                }),
+                EncryptingCredentials = enc
+            };
+
+            var tokenCreated = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenJwe = tokenHandler.WriteToken(tokenCreated);
+            Console.WriteLine(tokenJwe);
+        }
+
+        private static async Task<SecurityKey> Loadkey()
+        {
+            var publicKey = await _client.GetStringAsync("http://localhost:5000/jwks_e");
+            var key = JsonWebKeySet.Create(publicKey);
+            return key.Keys.FirstOrDefault();
+        }
+    }
+}
---
+++ Server/Controllers/DecryptController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using NetDevPack.Security.Jwt.Interfaces;
+using Server.Models;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DecryptController : ControllerBase
+    {
+        private readonly ILogger<DecryptController> _logger;
+        private readonly IJsonWebKeySetService _jsonWebKeySetService;
+
+        public DecryptController(ILogger<DecryptController> logger, IJsonWebKeySetService jsonWebKeySetService)
+        {
+            _logger = logger;
+            _jsonWebKeySetService = jsonWebKeySetService;
+        }
+
+        [HttpPost]
+        public IActionResult GetResultData(EncryptedDataRequestDto encryptedData)
+        {
+            try
+            {
+                var handler = new JsonWebTokenHandler();
+                var encryptingCredentials = _jsonWebKeySetService.GetCurrentEncryptingCredentials();
+                var result = handler.ValidateToken(encryptedData.TokenJwe,
+                    new TokenValidationParameters
+                    {
+                        ValidIssuer = "issuer",
+                        ValidAudience = "audience",
+                        RequireSignedTokens = false,
+                        TokenDecryptionKey = encryptingCredentials.Key,
+                    });
+
+                if (!result.IsValid)
+                    BadRequest();
+
+                return Ok(result.Claims);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Erro inesperado.");
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
